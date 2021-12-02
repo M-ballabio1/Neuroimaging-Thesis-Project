39211687b9d8869f810050d79b24b5f355a95c27
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2021-12-02 14:30:52 +0100

    update
    minor changes

--- 0.Pre-processing and Segmentation/multi_otsu_segm.py
+++ 0.Pre-processing and Segmentation/multi_otsu_segm.py
@@ -6,46 +6,50 @@ Created on Tue Nov 30 09:15:49 2021
 @author: matteo99
 """
 
-# Image segmentation and morphological operators
-
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 from skimage.filters import threshold_multiotsu
 import cv2
 import os
-import seaborn as sns
 import nibabel as nib
 from nibabel.testing import data_path
 
-#img = cv2.imread("/home/matteo99/Scaricati/maff_whole_brain_Crop.png", 0)
+#%% Path
 
-immagine_ni1 = os.path.join('/home/matteo99/Scrivania/MAFF_T1W3D_N4_reoriented.nii.gz')
+#img = cv2.imread("/home/matteo99/Scaricati/maff_whole_brain_Crop.png", 0)
+immagine_ni1 = os.path.join('/home/matteo99/Scrivania/SUBJECT_for_otsu_script/SUBJECT_323340_T1W3D_reoriented_N4.nii.gz')
+gt = os.path.join('/home/matteo99/Scrivania/SUBJECT_for_otsu_script/SUBJECT_323340_T1W3D_Segmentazione_Completa_4_labels.nii.gz')
+
+#%% Load nifti file and histogram
+
 n1_img = nib.load(immagine_ni1)
 array_nifti = n1_img.get_fdata()
 #n = int(array_nifti.shape[2]/2)
-array2D = array_nifti[:,:,90]
+array2D = array_nifti[:,:,90]  #Permette di estrarre una slices assiale (bastava cambiare posizione del 90 per estrarre assiale)
 #plt.grid(b=None)
 plt.axis('off')
 plt.imshow(array2D, cmap='gray')
-
 Array_flattizzato = array2D.flatten()
-'''
+
+
 plt.figure(figsize=(30,20))
 plt.hist(Array_flattizzato, bins=40, density=True)
-plt.xlim(9500,70000)
-plt.ylim(0,0.00002)
+plt.grid()
+plt.xlim(0,145000)
+plt.ylim(0,0.00012)
 plt.show()
-'''
 
-####AUTO###########################
-# Apply multi-Otsu threshold 
+
+
+#%% Segmentation method Multi-Otsu threshold 
 
 thresholds = threshold_multiotsu(array2D, classes=4)
 
 # Digitize (segment) original image into multiple classes.
 #np.digitize assign values 0, 1, 2, 3, ... to pixels in each class.
 regions = np.digitize(array2D, bins=thresholds)
-plt.imshow(regions)
+#plt.imshow(regions)
 
 segm1 = (regions == 0)
 segm2 = (regions == 1)
@@ -59,7 +63,7 @@ segm4 = (regions == 3)
 
 from scipy import ndimage as nd
 
-segm1_opened = nd.binary_opening(segm1, np.ones((1,1)))
+segm1_opened = nd.binary_opening(segm1, np.ones((1,1)))     #Definizione grandezza voxels segmentations
 segm1_closed = nd.binary_closing(segm1_opened, np.ones((1,1)))
 
 segm2_opened = nd.binary_opening(segm2, np.ones((1,1)))
@@ -78,7 +82,51 @@ all_segments_cleaned[segm2_closed] = (1,0,0)
 all_segments_cleaned[segm3_closed] = (0,1,0)
 all_segments_cleaned[segm4_closed] = (0,0,1)
 
-plt.axis('off')
+#plt.axis('off')
 plt.figure(figsize=(100,100))
 plt.imshow(all_segments_cleaned)  #All the noise should be cleaned now
-plt.imsave("/home/matteo99/Scrivania/Segm_otsu.jpg", all_segments_cleaned) 
+plt.imsave("/home/matteo99/Scrivania/Segm_otsu.jpg", all_segments_cleaned)
+
+#%% Load Segmentation ground truth
+
+def loadArray(fullpath):    
+	file_ext=fullpath[fullpath.rfind('.'):]
+	if file_ext == ".gz" or file_ext == ".nii":
+	    	
+		NiiStructure = nib.load(fullpath)
+		Array = NiiStructure.get_data()
+	    		
+	elif file_ext == ".npy":
+	    	
+	 	Array=np.load(fullpath)
+	else:	
+		print("not valid extension")
+		return []
+	return Array
+
+NiiArray1 = loadArray(gt)
+Segm_array2D = NiiArray1[:,:,90] #estrae stessa slices
+
+#%% Dice Score
+
+def compute_dice(truth, pred, classes):
+    dice_scores = []
+    # Compute Dice for each class
+    for i in range(classes):
+        try:
+            pd = np.equal(pred, i)
+            gtr = np.equal(truth, i)
+            dice = 2*np.logical_and(pd, gtr).sum()/(pd.sum() + gtr.sum())
+            dice_scores.append(dice)
+        except ZeroDivisionError:
+            dice_scores.append(0.0)
+    # Return computed Dice scores
+    return dice_scores
+
+Dice = compute_dice(Segm_array2D, regions, 4)
+print(Dice)
+Dice_new = np.reshape(Dice, (1,4))
+
+# Graft our results matrix into pandas data frames 
+overlap_results_df = pd.DataFrame(data=Dice, columns=('background','CSF','GM',
+                                                          'WM'), index=['Sub1'])
